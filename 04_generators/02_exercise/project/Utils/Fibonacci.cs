24d0d7b9fd04f2eb8e42d54b4b54929e286c4891
Maksymilian Wójcik <maksymilian.b.wojcik@gmail.com>
2020-10-09 17:54:47 +0200

    Fibonacci % RangeFibonacci

--- 04_generators/02_exercise/project/Utils/Fibonacci.cs
+++ 04_generators/02_exercise/project/Utils/Fibonacci.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Utils
 {
@@ -7,7 +9,19 @@ namespace Utils
     {
         public virtual IEnumerable<int> Numbers()
         {
-            // TODO: ...
+            // TODO: ...    
+
+            var prev = -1;
+            var res = 1;
+            for (int i = 0; i<44; i++)
+            {
+                int sum = res + prev;
+                prev = res;
+                res = sum;
+                yield return sum;
+            }
+
+            throw new IndexOutOfRangeException();
         }
     }
 }
--- 04_generators/02_exercise/project/Utils/RangeFibonacci.cs
+++ 04_generators/02_exercise/project/Utils/RangeFibonacci.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace Utils
 {
@@ -6,11 +8,40 @@ namespace Utils
     {
         // TODO: Fields....
 
+        private int start;
+        private int step;
+        private int till;
+
         // TODO: Constructor....
 
+        public RangeFibonacci()
+        {
+            start = 0;
+            step = 1;
+            till = 1;
+        }
+
+        public RangeFibonacci(int a = 0, int b = 1, int stop = 1)
+        {
+            start = a;
+            step = b;
+            till = stop;
+        }
+        
         public override IEnumerable<int> Numbers()
         {
             // TODO: Implement using version from base class....
+
+            var prev = -1;
+            var res = 1;
+            for (int i = 0; i<=44; i++)
+            {
+                int sum = res + prev;
+                prev = res;
+                res = sum;
+                if (i <= till && i >= start && (i-start)%step==0 )
+                    yield return sum;
+            }
         }
     }
 }
